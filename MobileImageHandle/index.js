8ce5f473289d75ba97f53760bcd864c7aaef3edf
Manda Kosola <t6koma04@students.oamk.fi>
2018-11-29 23:37:52 +0200

    Kuvan lisääminen henkilölle käyttäjän id:n avulla, toimii

--- MobileImageHandle/index.js
+++ MobileImageHandle/index.js
@@ -1,43 +1,35 @@
-
+// https://facedatabasetest.azurewebsites.net/api/MobileTrigger
 const axios = require('axios');
 
 const axiosCustom = axios.create({
     baseURL: 'https://northeurope.api.cognitive.microsoft.com/face/v1.0',    
     headers: {
         'Content-Type': 'application/json',
-        'Ocp-Apim-Subscription-Key': process.env['FaceApiAccessKey']         
-    }
-});
-
-// For adding binary image to Face
-const axiosCustomBinary = axios.create({
-    baseURL: 'https://northeurope.api.cognitive.microsoft.com/face/v1.0',    
-    headers: {
-        'Content-Type': 'application/octet-stream',
-        'Ocp-Apim-Subscription-Key': process.env['FaceApiAccessKey']         
+        'Ocp-Apim-Subscription-Key': process.env["FaceApiAccessKey"]         
     }
 });
 
 // Blob storage settings
 var azureStorage = require('azure-storage');
-const storageAccount = process.env[StorageAccount];
-const storageAccessKey = process.env[StorageAccessKey];
-const containerName = process.env[ContainerName];
+const storageAccount = process.env.StorageAccount;
+const storageAccessKey = process.env.StorageAccessKey;
+const containerName = process.env.ContainerName;
 var hostName = 'https://' + storageAccount + '.blob.core.windows.net';
 var blobService = azureStorage.createBlobService(storageAccount, storageAccessKey);
 
 // Set database configurations for connecting:
-var Connection = require('tedious').Connection;
+/*var Connection = require('tedious').Connection;
 var Request = require('tedious').Request
 var TYPES = require('tedious').TYPES;
 
-var _currentData = {};
 var config = {
-    userName: process.env[DBusername],
-    password: process.env[DBpassword],
-    server: process.env[DBserver],
-    options: {encrypt: true, database: process.env[DBname]}
-};
+    userName: process.env.DBusername,
+    password: process.env.DBpassword,
+    server: process.env.DBserver,
+    options: {encrypt: true, database: process.env.DBname}
+};*/
+
+const dbconnector = require('../Shared/DatabaseFuncs.js');
 
 function uploadImage(req) {
 
@@ -46,7 +38,7 @@ function uploadImage(req) {
     const boundaryMarkerIndex = req.headers['content-type'].indexOf("=");
     const boundary = req.headers['content-type'].substring(boundaryMarkerIndex+1);
     const data = req.body;
-    console.log("Image data: " + req);
+    //console.log(req);
     // remove boundary from the end
     // Add 2 for boundary prefix --, add 2 for last boundary postfix --, add 2*2 for linebreaks
     let testData = data.slice(0,contentLength - (boundary.length + 2 + 2 + 2 + 2)); 
@@ -68,77 +60,34 @@ function uploadImage(req) {
     }
     // remove the multipart header
     const finalImageData = testData.slice(dataStartIndex);
+    //console.log(finalImageData);
     return finalImageData;
 }
 
-function saveImageToAzure(imgString, imgName) {
-    // imgString = binarydata
-    // imgName = userID.png 
-    // Check if imgName already exists:
-    let newImgName = imgName;
-    blobService.getBlobProperties(
-        containerName,
-        imgName,
-        function(err, properties, status) {
-            if (status.isSuccessful) {
-                // Blob exists
-                newImgName += "0"; // Add something to imgName to make it a new blob
-            } 
-        });
-    newImgName += ".png";
+function saveImageToAzure(imageData) {
+
+    // Create random name for image
+    const shortid = require('shortid');
+    const imgName = shortid.generate();
+    newImgName = imgName + ".png";
+    console.log("image name: " + newImgName);
+
+    // Create Blob
     blobService.createBlockBlobFromText(
         containerName,
         newImgName,
-        imgString,
+        imageData,
         function(err, result, response) {
             if(err){
                 throw err;
             }
             console.log(["Uploaded image to " + containerName + " container on Azure."].join(''));
-            //console.log(["URL: ", publicUrl].join(''));
         });
-    //Return link to blob
+
+    //Return link to blob we just created
     return blobService.getUrl(containerName, newImgName, null, hostName);
 }
 
-// Convert base64 to binary buffer:
-/*var rawdata = req.body;
-var matches = rawdata.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-var type = matches[1];
-var buffer = new Buffer(matches[2], 'base64');
-
-blobsrv.createBlockBlobFromText('mycontainer','testimage.jpg', buffer, {contentType:type}, function(error, result, response) {
-        if (error) {
-            console.log(error);
-        }else{
-         console.log(result)
-        }
-    });*/
-
-    /*async function createPersonGroup(personGroupId)
-    {
-        let returnData = {
-            status: null,
-            data: null
-        };
-    
-        try {
-            const response = await axiosCustom.put(`/persongroups/${personGroupId}`,
-                {
-                    name: "Persons"
-                });
-    
-            returnData.status = response.status;
-            returnData.data = response.data;
-        }
-        catch(error)
-        {
-            returnData.status = error.response.status;
-            returnData.data = error.response.data;
-        }
-        return returnData;
-    }*/
-    
 async function createPerson(personGroupId, personData)
 {
     console.log("personGroupID", personGroupId);
@@ -210,7 +159,7 @@ async function trainPersonGroup(personGroupId)
     return returnData;
 }
 
-async function dbQuery(query) {
+/*async function dbQuery(query) {
 
     // Connect to database
     var connection = new Connection(config);
@@ -223,15 +172,6 @@ async function dbQuery(query) {
         console.log(err);}
     });
 
-    /*request.on('row', function(columns) {
-        // prints ID and name to log
-        _currentData.ID = columns[0].value;
-        _currentData.FirstName = columns[1].value;
-        _currentData.LastName = columns[2].value;
-        _currentData.GroupID = columns[3].value;
-        console.log(_currentData);
-    });*/
-
     request.on('requestCompleted', function () {
         console.log('Request completed.');
     });
@@ -239,18 +179,45 @@ async function dbQuery(query) {
 });
 }
 
+async function dbQueryGet(query, callback) {
+
+    var result = "";
+
+    // Connect to database
+    var connection = new Connection(config);
+    connection.on('connect', function(err) {
+        console.log("Connected to database");
+
+        // Make a request to database
+        var request = new Request(query, function (err, rowCount) {
+            if (err) {
+                callback(err);
+            } else {
+                if (rowCount < 1) {
+                    callback(null, null);
+                } else {
+                    callback(null, result);
+                }
+            }
+        });
+        request.on("row", function(rowObject) {
+            // populate the results array
+            result.push(rowObject);
+        });
+
+        connection.execSql(request);
+    });
+}*/
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    // TODO: get user's id from mobile app request
-    let userIdString = "2"; // testing with id 1 or 2
-
+    let userId = req.headers['user'];    // get user's id from request
+    context.log("userid from header:", userId);
     let imgData = uploadImage(req); // Get image from request
-    let imgUrl = saveImageToAzure(imgData, userIdString); // Upload image to Azure Blob Storage and get url
+    let imgUrl = saveImageToAzure(imgData);   // Upload image to Azure Blob Storage and get url
 
     const personGroupId = '1'; // Set the GroupID
-    let personIdString = "";
 
     // Create person group
     // https://northeurope.api.cognitive.microsoft.com/face/v1.0/persongroups/{personGroupId}
@@ -264,17 +231,41 @@ module.exports = async function (context, req) {
     context.log("Person group create status " + createPersonGroupResponse.status);
     context.log("Person group create data " + createPersonGroupResponse.data);*/
 
-    // * Create new Person *
-    const personCreateResponse = await createPerson(personGroupId, { name: 'Max Tester'});
-    context.log("Person create status " + personCreateResponse.status);
-    context.log("Person create data " + personCreateResponse.data);
+    var personIdString = null;
 
-    //  * Get Persons id *
-    personIdString = personCreateResponse.data.personId;
-    context.log("personid:", personIdString);
-    // Set personId and imgUrl to database, using user ID:
-    dbQuery("UPDATE dbo.Persons SET PersonID = '" + personIdString + 
-            "', ImageURL = '" + imgUrl + "' WHERE ID = '" + userIdString + "'");
+    // Find PersonID from database, if it doesn't exists: create new Face API Person
+    try {
+        var query = await dbconnector.querydb(context, "SELECT PersonID FROM dbo.Persons WHERE ID = '"+ 
+                userId +"'");
+        personIdString = query[0].PersonID;
+        context.log(personIdString);
+    }
+    catch(err){
+        context.log(err);
+    }
+
+    if (personIdString === null) {
+        // * Create new Person *
+        const personCreateResponse = await createPerson(personGroupId, { name: userId });
+        context.log("Person create status ", personCreateResponse.status);
+        context.log("Person create data ", personCreateResponse.data);
+
+        //  * Get Persons id *
+        personIdString = personCreateResponse.data.personId;
+        context.log("personid:", personIdString);
+        try {
+        // Set personId and imgUrl to database (Images table)
+        var query = await dbconnector.querydb(context, "INSERT INTO dbo.Images (ImageURL, PersonsID) VALUES ('"+
+                imgUrl +"', '"+ userId +"')");
+        var query = await dbconnector.querydb(context, "UPDATE dbo.Persons SET PersonID = '" + personIdString + 
+        "' WHERE ID = '" + userId + "'");
+        }
+        catch(err){
+            context.log(err);
+
+        }
+    }
+
 
     // * Add face to Person *
     //const filePath = "https://cdn.pixabay.com/photo/2018/10/18/19/02/woman-3757184_960_720.jpg";
@@ -286,8 +277,8 @@ module.exports = async function (context, req) {
 
     // * Train Person Group *
     const trainResponse = await trainPersonGroup(personGroupId);
-    context.log("Train status " + trainResponse.status);
-    context.log("Train data " + trainResponse.data);
+    context.log("Train status ", trainResponse.status);
+    context.log("Train data ", trainResponse.data);
     
     if(trainResponse.status == 202)
     {
@@ -295,8 +286,7 @@ module.exports = async function (context, req) {
     }
 
     context.res = {
-        status: trainResponse.status,
-        body: trainResponse.data
+        body: req.body
     };
     
 
