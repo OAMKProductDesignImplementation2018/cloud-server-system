804a6b0e407f8861f34375d99669dd4e249e213e
Vili <wilssoni@gmail.com>
2018-12-05 18:06:02 +0200

    Suolaus cryp.js tehdään siten että sen saa erilliseen tietokanta sarakkeeseen
    Signed-off-by: Vili <wilssoni@gmail.com>

--- Shared/crypt.js
+++ Shared/crypt.js
@@ -1,22 +1,24 @@
 const crypto = require('crypto');
 
+function generateSalt(){
+  return crypto.randomBytes(16).toString('hex');
+}
+
 // Create password hash using Password based key derivative function 2
 function hashPassword(password) {
-   const salt = crypto.randomBytes(16).toString('hex');
+   const salt = generateSalt();
    const hash = crypto.pbkdf2Sync(password, salt, 2048, 32, 'sha512').toString('hex');
-   return [salt, hash].join('$');
+   return [salt, hash];
 }
 
 // Checking the password hash
-function verifyHash(password, original) {
-  const originalHash = original.split('$')[1];
-  const salt = original.split('$')[0];
+function verifyHash(password, original, salt) {
   const hash = crypto.pbkdf2Sync(password, salt, 2048, 32, 'sha512').toString('hex');
-
-  return hash === originalHash
+  return hash === original
 }
 
 module.exports = {
   hashP = hashPassword,
-  checkP = verifyHash
+  checkP = verifyHash,
+  salt = generateSalt
 }
