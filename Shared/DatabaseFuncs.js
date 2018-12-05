080759871c76488bb2464f866a89f5c35e02fb97
Vili <wilssoni@gmail.com>
2018-12-05 19:29:43 +0200

    DatabaseFuncs: Henkilötietoja haulle ja uudelle käyttäjälle oma funktio
    Toiminnot myös sql-injektio suojattu.
    
    Signed-off-by: Vili <wilssoni@gmail.com>

--- Shared/DatabaseFuncs.js
+++ Shared/DatabaseFuncs.js
@@ -43,6 +43,61 @@ function doquery(context, query)
   });
 }
 
+function doqueryWithCustomRequest(context, request)
+{
+  return new Promise(function(resolve, reject) {
+    var _currentData = [];
+    var connection = new Connection(config);
+    try{
+      connection.on('connect', function(err) {
+        context.log("Connected");
+        request.on('row', function(columns) {
+          var tempObject = {};
+          columns.forEach(function(element) {
+            tempObject[element.metadata.colName] = element.value;
+          }, this);
+          _currentData.push(tempObject);
+        });
+        request.on('requestCompleted', function () {
+          context.log('Request completed.');
+          resolve(_currentData);
+        });
+        connection.execSql(request);
+      });
+    }
+    catch(error){reject(error);}
+  });
+}
+
+function findPersonDetails(context, personid)
+{
+  const query = "SELECT FirstName, LastName, GroupID FROM dbo.Persons WHERE PersonID = @PersonID";
+
+  var request = new Request(query, function(err) {
+    if (err) {
+      context.log(err);}
+  });
+  request.addParameter("PersonID", TYPES.VarChar, personid);
+  return doqueryWithCustomRequest(context, request);
+}
+
+function insertUser(context, fname, lname, email, pword)
+{
+  const query = "INSERT INTO dbo.Persons (FirstName, LastName, Email, Pword) VALUES (@FirstName, @LastName, @Email, @Pword)";
+  var request = new Request(query, function(err) {
+    if (err) {
+      context.log(err);}
+  });
+  request.addParameter("FirstName", TYPES.VarChar, fname);
+  request.addParameter("LastName", TYPES.VarChar, lname);
+  request.addParameter("Email", TYPES.VarChar, email);
+  request.addParameter("Pword", TYPES.VarChar, pword);
+  //request.addParameter("Salt", TYPES.VarChar, salt);
+  return doqueryWithCustomRequest(context, request);
+}
+
 module.exports = {
-  querydb: doquery
+  querydb: doquery,
+  findperson: findPersonDetails,
+  newuser: insertUser
 };
