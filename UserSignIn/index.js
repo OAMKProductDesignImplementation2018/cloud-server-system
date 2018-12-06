1f7c88694d7de244840823a373d24fa5e6901118
Manda Kosola <t6koma04@students.oamk.fi>
2018-12-07 00:58:17 +0200

    Tietokantakäskyt turvallisemmiksi ja mobiililaitteelle palautuksena käyttäjän tiedot

--- UserSignIn/index.js
+++ UserSignIn/index.js
@@ -1,5 +1,21 @@
 // Set database configurations for connecting:
 const dbconnector = require('../Shared/DatabaseFuncs.js');
+const crypt = require('../Shared/Crypt.js');
+
+function parsePerson(data){
+
+    return {
+        "id" : data[0].ID,
+        "firstname" : data[0].FirstName,
+        "lastname" : data[0].LastName,
+        "groupid" : data[0].GroupID,
+        "gender" : data[0].Gender,
+        "imageurl" : data[0].ImageURL,
+        "gender" : data[0].Gender,
+        "organisation" : data[0].Organisation,
+        "email" : data[0].Email
+    }
+}
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
@@ -17,22 +33,32 @@ module.exports = async function (context, req) {
     context.log(pw);
 
     if (email && pw) {
-        // Set personId and imgUrl to database, using user ID:
 
         try {
-            var querySelect = await dbconnector.querydb(context, 
-                "SELECT * FROM dbo.Persons WHERE Email = '" + email +
-                 "' AND Pword = '" + pw + "'");
+            var querySelect = await dbconnector.login(context, email);
             context.log(querySelect);
 
             if(querySelect){
-                var userData = querySelect[0];
-            
-                context.log(userData);
-                context.res = {
-                    status: 200,
-                    body: { userData }
-                };
+                var password = querySelect[0].Pword;
+                var salt = querySelect[0].Salt;
+
+                var loginSuccess = crypt.checkP(pw, password, salt);
+                if(loginSuccess){
+                    var personData = await dbconnector.findAllData(context, email);
+                    var returnedData = parsePerson(personData);
+
+                    context.res = {
+                        status: 200,
+                        body: { "login" : "Success",
+                                "data" : returnedData }
+                    };
+                }
+                else {
+                    context.res = {
+                        status: 400,
+                        body: { "login" : "Fail" }
+                    };
+                }
             }
         }
         catch(err){
@@ -42,7 +68,7 @@ module.exports = async function (context, req) {
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Please insert email and password."
         };
     }
 };
